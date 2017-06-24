0d6c8ac213265020d10da15eb49591d5f0657b42
zyx <wjszzyx@sohu.com>
2017-06-24 09:40:06 +0800

    cross validation

--- lsClassifier.py
+++ lsClassifier.py
@@ -2,10 +2,11 @@ from __future__ import division
 import numpy as np
 from getFeature import X, y
 
+
 def lsClassifier(traindata, trainlabel, testdata, testlabel, lamda):
     n = traindata.shape[0]
     m = traindata.shape[1]
-    print n, m
+    # calculate weights and b
     ones = np.ones((n, 1))
     X = np.column_stack((ones, traindata))
     X = np.mat(X)
@@ -20,20 +21,40 @@ def lsClassifier(traindata, trainlabel, testdata, testlabel, lamda):
     n1 = testdata.shape[0]
     ones = np.ones((n1, 1))
     testdata = np.column_stack((ones, testdata))
-    ypred = testdata.dot(w)
-    count_correct = 0
+    y_pred = testdata.dot(w)
+    correct_count = 0
     for i in xrange(n1):
-        if ypred[i] > 0:
-            ypred[i] = 1
+        if y_pred[i] > 0:
+            y_pred[i] = 1
         else:
-            ypred[i] = -1
-        if ypred[i] == testlabel[i]:
-            count_correct += 1
-    print count_correct / n1
+            y_pred[i] = -1
+        if y_pred[i] == testlabel[i]:
+            correct_count += 1
+    return y_pred, correct_count / n1
+
 
 n = X.shape[0]
+foldsize = n // 5
+with open("cross_validation.txt", "w") as f:
+    for lamda in [1e-4, 0.01, 0.1, 0.5, 1, 5, 10, 100, 1000, 5000, 10000]:
+        f.write("when lambda is {}, ".format(lamda))
+        avg_accuracy = 0.0
+        for i in xrange(5):
+            begin = i * foldsize
+            end = begin + foldsize
+            traindata = np.row_stack((X[:begin, :], X[end:, :]))
+            trainlabel = np.concatenate((y[:begin], y[end:]))
+            testdata = X[begin:end, :]
+            testlabel = y[begin:end]
+            (y_pred, accuracy) = lsClassifier(traindata, trainlabel, testdata, testlabel, lamda)
+            avg_accuracy += accuracy
+        avg_accuracy /= 5
+        f.write("the average accuracy is {}.\n".format(avg_accuracy))
+
+'''
 traindata = X[0:int(0.8 * n), :]
 trainlabel = y[0:int(0.8 * n)]
 testdata = X[int(0.8 * n):, :]
 testlabel = y[int(0.8 * n):]
 lsClassifier(traindata, trainlabel, testdata, testlabel, 0.1)
+'''
